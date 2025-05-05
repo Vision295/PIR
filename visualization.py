2588453d92681f9de16a51bef8d96e2e0149eabf
Vision295 <theophilewachtel@gmail.com>
2025-05-05 09:21:07 +0200

    affichage en fonction de l'ordre croissant ou décroissant selon dist euclidienne ou dist cosine

--- visualization.py
+++ visualization.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class Visualization:
-      def __init__(self, file_path):
+      def __init__(self, file_path, ascending=True):
             self.file_path = file_path
             self.df = None
+            self.ascending = ascending
 
       def load_data(self):
             self.df = pd.read_csv(self.file_path)
@@ -22,15 +23,16 @@ class Visualization:
 
       def top_values(self, column_name, n=5):
             if self.df is not None:
-                  top = self.df.nsmallest(n, 'text-classification')
+                  top = self.df.sort_values('text-classification', ascending=self.ascending).head(n)
+                  print(self.file_path)
                   print(top)
             else:
                   print("Data not loaded yet.")
 
 vizualizer = [
-      Visualization('csv/dataset0-similarityFunc0.csv'),
-      Visualization('csv/dataset0-similarityFunc1.csv'),
-      Visualization('csv/dataset0-similarityFunc2.csv')
+      Visualization('csv/dataset0-similarityFunc0.csv', ascending=False),
+      Visualization('csv/dataset0-similarityFunc1.csv', ascending=True),
+      Visualization('csv/dataset0-similarityFunc2.csv', ascending=True)
 ]
 for viz in vizualizer:
       viz.load_data()
